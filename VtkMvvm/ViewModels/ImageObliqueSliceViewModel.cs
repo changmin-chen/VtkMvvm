85ba96ddc2e7b66f7071f0564b2dd0a464a3d3da
Chang-Min, Chen <47084071+changmin-chen@users.noreply.github.com>
2025-06-26 16:02:12 +0800

    [rev] move QuaternionExtensions to the internal helper extension

--- VtkMvvm/ViewModels/ImageObliqueSliceViewModel.cs
+++ VtkMvvm/ViewModels/ImageObliqueSliceViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Numerics;
 using Kitware.VTK;
+using VtkMvvm.Extensions;
 using VtkMvvm.Features.Builder;
 using VtkMvvm.Models;
 
@@ -68,13 +69,17 @@ public class ImageObliqueSliceViewModel : VtkElementViewModel
     {
         q.ToAxisAngle(out Vector3 axis, out float angle);
 
-        vtkTransform? tf = vtkTransform.New();
+        using vtkTransform? tf = vtkTransform.New();
         tf.Identity();
         tf.RotateWXYZ(angle, axis.X, axis.Y, axis.Z);
         vtkMatrix4x4 rot = tf.GetMatrix();
         for (int r = 0; r < 3; ++r)
-        for (int c = 0; c < 3; ++c)
-            _axes.SetElement(r, c, rot.GetElement(r, c));
+        {
+            for (int c = 0; c < 3; ++c)
+            {
+                _axes.SetElement(r, c, rot.GetElement(r, c));
+            }
+        }
 
         // translation handled in SetSliceIndex
         _axes.SetElement(3, 0, 0);
@@ -102,43 +107,5 @@ public class ImageObliqueSliceViewModel : VtkElementViewModel
         _reslice.Modified(); // forces recompute next Render()
         Actor.Modified();
         OnModified();
-    }
-}
-
-public static class QuaternionExtensions
-{
-    /// <summary>
-    ///     Converts a System.Numerics.Quaternion to its axis-angle representation.
-    /// </summary>
-    /// <param name="quaternion">The quaternion to convert.</param>
-    /// <param name="axis">The output axis of rotation.</param>
-    /// <param name="angle">The output angle of rotation in degrees.</param>
-    public static void ToAxisAngle(this Quaternion quaternion, out Vector3 axis, out float angle)
-    {
-        // It's recommended to normalize the quaternion first.
-        quaternion = Quaternion.Normalize(quaternion);
-
-        // Calculate the angle.
-        // The quaternion's W component is cos(angle / 2).
-        angle = 2 * (float)Math.Acos(quaternion.W);
-
-        // Calculate the axis.
-        float s = (float)Math.Sqrt(1.0f - quaternion.W * quaternion.W);
-
-        if (s < 0.001f)
-        {
-            // If s is close to zero, the axis is not well-defined.
-            // This happens when the angle is close to 0 or 360 degrees.
-            // In this case, we can default to any arbitrary axis, for example, the X-axis.
-            axis = new Vector3(1.0f, 0.0f, 0.0f);
-        }
-        else
-        {
-            // The axis is the vector part of the quaternion divided by sin(angle / 2).
-            axis = new Vector3(quaternion.X / s, quaternion.Y / s, quaternion.Z / s);
-        }
-
-        // Convert angle from radians to degrees.
-        angle = angle * (180.0f / (float)Math.PI);
     }
 }
