bfa58e72eb2af06b37f296e7dba506be3f901613
Chang-Min, Chen <47084071+changmin-chen@users.noreply.github.com>
2025-07-09 11:32:38 +0800

    [add] introduce test window for disposal

---
+++ PresentationTest/ViewModels/DisposalTestWindowViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System.Reactive;
+using System.Reactive.Disposables;
+using PresentationTest.TestData;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using VtkMvvm.Features.Builder;
+using VtkMvvm.Models;
+using VtkMvvm.ViewModels;
+using MessageBox = System.Windows.MessageBox;
+
+namespace PresentationTest.ViewModels;
+
+public class DisposalTestWindowViewModel : ReactiveObject
+{
+    private const string ImagePath = @"TestData\CT_Abdo.nii.gz";
+    private readonly SerialDisposable _serialDisposable = new();
+    [Reactive] public IReadOnlyList<ImageOrthogonalSliceViewModel> AxialVms { get; private set; }
+    public ReactiveCommand<Unit, Unit> LoadNewImageCommand { get; }
+
+    public DisposalTestWindowViewModel()
+    {
+        LoadNewImageCommand =  ReactiveCommand.CreateFromTask(SwapNewImage);
+        
+        var image = TestImageLoader.ReadNifti(ImagePath);
+        var bgPipe = ColoredImagePipelineBuilder.WithSharedImage(image).Build();
+        var axialVm = new ImageOrthogonalSliceViewModel(SliceOrientation.Axial, bgPipe);
+        
+        AxialVms = [axialVm];
+        _serialDisposable.Disposable = axialVm;  // stash for next disposal
+    }
+
+    private async Task SwapNewImage()
+    {
+        var image = await Task.Run(() => TestImageLoader.ReadNifti(ImagePath));
+        var bgPipe = ColoredImagePipelineBuilder.WithSharedImage(image).Build();
+        var axialVm = new ImageOrthogonalSliceViewModel(SliceOrientation.Axial, bgPipe);
+        
+        AxialVms = [axialVm];
+        _serialDisposable.Disposable = axialVm;
+
+        MessageBox.Show("Loaded new image.");
+    }
+}
---
+++ PresentationTest/Views/DisposalTestWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Reactive.Disposables;
+using System.Windows;
+using Kitware.VTK;
+using PresentationTest.ViewModels;
+using VtkMvvm.Features.InteractorBehavior;
+
+namespace PresentationTest.Views;
+
+public partial class DisposalTestWindow : Window
+{
+    private readonly CompositeDisposable _disposables = new();
+    private DisposalTestWindowViewModel _vm;
+    public DisposalTestWindow()
+    {
+        InitializeComponent();
+        Loaded += OnLoadedOnce;
+    }
+
+    private void OnLoadedOnce(object sender, RoutedEventArgs e)
+    {
+        Loaded -= OnLoadedOnce;
+        if (DataContext is DisposalTestWindowViewModel vm)
+        {
+            _vm = vm;
+        }
+        
+        // scroll-to-next-slice 
+        vtkRenderWindowInteractor iren = AxialControl.Interactor;
+        var style = vtkInteractorStyleImage.New();
+        MouseInteractorBuilder.Create(iren, style)
+            .Scroll(forward =>
+            {
+                int increment = forward ? 1 : -1;
+                _vm.AxialVms[0].SliceIndex += increment;
+            })
+            .Build()
+            .DisposeWith(_disposables);
+    }
+}
