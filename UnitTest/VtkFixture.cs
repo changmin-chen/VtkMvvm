a958931c16e79f0f8c09578590e4d5a864d484df
Chang-Min, Chen <47084071+changmin-chen@users.noreply.github.com>
2025-07-20 22:34:07 +0800

    [add] unit tests for `ImageObliqueSliceViewModel` and supporting VTK utilities

---
+++ UnitTest/VtkFixture.cs
@@ -0,0 +1,13 @@
+﻿namespace UnitTest;
+
+
+[CollectionDefinition("VTK")] public sealed class VtkCollection : ICollectionFixture<VtkFixture> { }
+
+public sealed class VtkFixture : IDisposable
+{
+    public VtkFixture()
+    {
+     
+    }
+    public void Dispose() { }
+}
